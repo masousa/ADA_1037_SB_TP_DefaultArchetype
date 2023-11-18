3707da787dc213d914351e7a6270c1f36593bcbf
Matheus Sousa <alvessousam6@gmail.com>
2023-11-17 23:12:33 -0300

    Initial commit

---
+++ src/main/java/ada/tech/tenthirty/tvpackages/DefaultArchetypeApplication.java
@@ -0,0 +1,13 @@
+package ada.tech.tenthirty.tvpackages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultArchetypeApplication.class, args);
+	}
+
+}
